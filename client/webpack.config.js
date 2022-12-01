183585f64a837eccdf91221605159a812496b795
HammontreeWebDev <hammontreewebdevelopment@gmail.com>
2022-12-01 16:42:31 -0500

    added asset cache to sw.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,8 +31,8 @@ module.exports = () => {
         background_color: '#7eb4e2',
         fingerprints: true,
         theme_color: '#7eb4e2',
-        start_url: './',
-        publicPath: './',
+        start_url: '/',
+        publicPath: '/',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
